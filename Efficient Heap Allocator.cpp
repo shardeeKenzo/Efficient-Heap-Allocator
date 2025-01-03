d07481bf2b2e5b3e0a293f697690c1bc30ac7588
Artem Novorosiuk <artem.novorosiuk@student.kdg.be>
2025-01-03 21:59:40 +0100

    -template for allocation

--- Efficient Heap Allocator.cpp
+++ Efficient Heap Allocator.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstddef>
+#include <string>
 
 constexpr std::size_t POOL_SIZE = 1024 * 1024 * 10; // 10 mb fixed-size
 
@@ -38,13 +39,14 @@ void print_memory_pool() {
 
 
 //allocation for integer object(make template function to allocate any type)
-void* allocate_memory_for_int(int data) {
+template <typename T>
+void* allocate_memory(T data) {
     auto* curr = reinterpret_cast<BlockHeader*>(memory_pool);
 
     //while curr is not nullptr
     while (curr) {
         //checks if current block is free + if it has enough space for allocation
-        if (curr->is_free && curr->size >= sizeof(int)) {
+        if (curr->is_free && curr->size >= sizeof(T)) {
             
             //change state to not free
             curr->is_free = false;
@@ -52,21 +54,21 @@ void* allocate_memory_for_int(int data) {
             //points to right location of memory that will be used(right next to the block metadata)
             void* usable_memory = reinterpret_cast<void*>(curr + 1);
             //says to treat specific raw memory on this location like int* and change initial bytes to bytes that we provided(I understand it like that)
-            *reinterpret_cast<int*>(usable_memory) = data;
+            *reinterpret_cast<T*>(usable_memory) = data;
 
             //calculate remaining memory
-            std::size_t remaining_size = curr->size - sizeof(int) - sizeof(BlockHeader);
+            std::size_t remaining_size = curr->size - sizeof(T) - sizeof(BlockHeader);
 
             //create new block if we have enough space so we can continue to use it
             if (remaining_size > 0) {
-                auto* new_block = reinterpret_cast<BlockHeader*>(reinterpret_cast<char*>(usable_memory) + sizeof(int));
+                auto* new_block = reinterpret_cast<BlockHeader*>(reinterpret_cast<char*>(usable_memory) + sizeof(T));
 
                 new_block->size = remaining_size;
                 new_block->is_free = true;
                 new_block->next = nullptr;
                 
                 curr->next = new_block;
-                curr->size = sizeof(int);
+                curr->size = sizeof(T);
             }
             
             return usable_memory;
@@ -79,8 +81,8 @@ void* allocate_memory_for_int(int data) {
     return nullptr;
 }
 
-void deallocate_memory_for_int(void* int_ptr) {
-    auto* curr = reinterpret_cast<BlockHeader*>(reinterpret_cast<char*>(int_ptr) - sizeof(BlockHeader));
+void deallocate_memory(void* ptr) {
+    auto* curr = reinterpret_cast<BlockHeader*>(reinterpret_cast<char*>(ptr) - sizeof(BlockHeader));
 
     if (curr->is_free == true) {
         std::cout << "Memory is already free" << std::endl;
@@ -102,17 +104,24 @@ int main()
     init_memory_pool();
     print_memory_pool();
 
-    int* number = (int*)allocate_memory_for_int(3);
+    char* number = (char*)allocate_memory('d');
 
     print_memory_pool();
     
-    int* number2 = (int*)allocate_memory_for_int(5);
+    int* number2 = (int*)allocate_memory(5);
     print_memory_pool();
 
-    int* number3 = (int*)allocate_memory_for_int(7);
+    double* number3 = (double*)allocate_memory(7.23);
 
-    deallocate_memory_for_int(number2);
-    deallocate_memory_for_int(number);
+    std::string* string = (std::string*)allocate_memory("Cat");
+
+    print_memory_pool();
+
+
+    deallocate_memory(number2);
+    deallocate_memory(number);
+
+
     print_memory_pool();
 
     return 0;
